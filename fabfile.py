796f4bccbb4f3b018ca99cf85a100d836ee52802
Roy Prins <prinsroy@gmail.com>
2015-03-17 08:50:00 +0100

    third try at fabric deployment

--- fabfile.py
+++ fabfile.py
@@ -3,14 +3,14 @@ from fabric.api import *
 
 
 def deploy():
-    env.freeze_folder = 'gh-pages'
-    env.freeze_branch = 'gh-pages'
+    freeze_folder = 'gh-pages'
+    freeze_branch = 'gh-pages'
 
     with prefix("source venv/bin/activate"):
         message = raw_input("Enter a git commit message:  ")
         local("git add -A && git commit -m \"%s\"" % message)
         local("git push origin master")
         local("python freeze.py")
-        with cd(env.freeze_folder):
-            run("git add -A && git commit -m \"%s\"" % message)
-            run("git push origin %s" % env.freeze_branch)
+        with cd(freeze_folder):
+            local("git add -A && git commit -m \"%s\"" % message)
+            local("git push origin %s" % freeze_branch)
