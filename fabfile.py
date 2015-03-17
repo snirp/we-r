0434b6010250c66ba324c3b96c6807f39a45881c
Roy Prins <prinsroy@gmail.com>
2015-03-17 09:34:01 +0100

    this time :/

--- fabfile.py
+++ fabfile.py
@@ -11,6 +11,6 @@ def deploy():
         local("git add -A && git commit -m \"%s\"" % message)
         local("git push origin master")
         local("python freeze.py")
-        with lcd('/Users/snirp/juis/snirpdrive/snirp/projecten/2015p002WeR-website/webproject/we-r/gh-pages/'):
-            local("git add -A && git commit -m \"%s\"" % message)
-            local("git push origin %s" % freeze_branch)
+    with lcd('/Users/snirp/juis/snirpdrive/snirp/projecten/2015p002WeR-website/webproject/we-r/gh-pages/'):
+        local("git add -A && git commit -m \"%s\"" % message)
+        local("git push origin %s" % freeze_branch)
