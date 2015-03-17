bff6916a572bcd29bc32a71716018d91ae7b3c37
Roy Prins <prinsroy@gmail.com>
2015-03-17 09:36:33 +0100

    cleaned up fabric deployment

--- fabfile.py
+++ fabfile.py
@@ -6,11 +6,11 @@ def deploy():
     freeze_folder = 'gh-pages'
     freeze_branch = 'gh-pages'
 
+    message = raw_input("Enter a git commit message:  ")
+    local("git add -A && git commit -m \"%s\"" % message)
+    local("git push origin master")
     with prefix("source venv/bin/activate"):
-        message = raw_input("Enter a git commit message:  ")
-        local("git add -A && git commit -m \"%s\"" % message)
-        local("git push origin master")
         local("python freeze.py")
-    with lcd('/Users/snirp/juis/snirpdrive/snirp/projecten/2015p002WeR-website/webproject/we-r/gh-pages/'):
+    with lcd(freeze_folder):
         local("git add -A && git commit -m \"%s\"" % message)
         local("git push origin %s" % freeze_branch)
